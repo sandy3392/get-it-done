065d9e4b2117dd27a3c010cef15bd1007da40ab5
Sandeep Vittala <applesandy33@gmail.com>
2021-12-19 15:45:57 -0500

    commiting logic for connecitng two pages

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,5 +1,19 @@
 var issueContainerEl = document.querySelector("#issues-container");
 var limitWarningEl = document.querySelector("#limit-warning");
+var repoNameEl = document.querySelector("#repo-name");
+
+
+var getRepoName = function(){
+  var queryString = document.location.search;
+  var repoName=  queryString.split("=")[1];
+  console.log(repoName);
+  if(repoName) {
+    repoNameEl.textContent = repoName;
+    getRepoIssues(repoName);
+  } else {
+    document.location.replace("./index.html");
+  }
+};
 
 var getRepoIssues = function(repo) {
     var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
@@ -18,9 +32,10 @@ var getRepoIssues = function(repo) {
           });
         }
         else {
-          alert("There was a problem with your request!");
+          // if not successful, redirect to homepage
+          document.location.replace("./index.html");
         }
-      });
+    });
 };
 
 var displayIssues = function(issues) {
@@ -69,4 +84,5 @@ var displayWarning = function(repo) {
     // append to warning container
     limitWarningEl.appendChild(linkEl);
 };
-getRepoIssues("facebook/react");
+
+getRepoName();
